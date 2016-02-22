9f7de86ecae23d7929aef83a16338bc57143ae8f
David Skeppstedt <david.skeppstedt@gmail.com>
2016-02-22 20:35:07 +0100

    initRoutes function, used by the NewRouter function to build the mux/router for the http server. This place serves as an easy way to add new routes. Also middleware supported added with alice. fix #4 Added the apiVersion functiont to test the routing. #2

---
+++ ptc/routes.go
@@ -0,0 +1,15 @@
+package ptc
+
+import (
+	"github.com/gorilla/context"
+	"github.com/justinas/alice"
+)
+
+func initRoutes() (routes Routes) {
+	middleware := alice.New(context.ClearHandler)
+
+	routes.Get("api version", "/api/1",
+		middleware.ThenFunc(apiVersion))
+
+	return
+}
