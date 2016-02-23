c7b775c0fe76863aec2a06abb35896eade8d0deb
David Skeppstedt <david.skeppstedt@gmail.com>
2016-02-23 22:29:20 +0100

    When the application is started we now instanciate a variable of the type Database that reads from my database configuration file and connects to the database called dev. After this setup functions can call all the implemented querying functions that the database type have been extended with. fix #6

--- ptc/server.go
+++ ptc/server.go
@@ -2,10 +2,20 @@ package ptc
 
 import (
 	"log"
+
 	"net/http"
+
+	"github.com/lcd/PTC-search-service/db"
 )
 
+var database db.Database
+
 func StartServer(port string) {
+	var err error
+	database, err = db.NewDatabase("./config/db.json", "dev")
+	if err != nil {
+		log.Fatal(err)
+	}
 	router := NewRouter()
 	log.Println("starting the webserver...", "http://localhost"+port)
 	http.ListenAndServe(port, router)
