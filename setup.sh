0c2f3432e0fd4fa887880581ca584a5dbf453549
Andreas Brommund <andreas@brommund.se>
2016-03-15 14:27:32 +0100

    Add a setup file

---
+++ setup.sh
@@ -0,0 +1,26 @@
+echo 'go get gorilla/context'
+go get github.com/gorilla/context
+
+echo 'go get julienschmidt/httprouter'
+go get github.com/julienschmidt/httprouter
+
+echo 'go get justinas/alice'
+go get github.com/justinas/alice
+
+echo 'go get justinas/lib/pq'
+go get github.com/lib/pq
+
+echo 'Add config folder'
+mkdir config
+
+echo 'Add db config file'
+echo '{
+	"dev": {
+		"database": "postgres",
+		"user": "",
+		"password": "",
+		"host": "127.0.0.1",
+		"port": "5432",
+		"ssl_mode": "disable"
+	}
+}' > config/db.json
