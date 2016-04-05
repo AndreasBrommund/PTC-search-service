a79c4a52115e339962efe56d401bc58b0e81da5a
David Skeppstedt <david.skeppstedt@gmail.com>
2016-04-05 17:55:08 +0200

    renamed controller.go to api.go

---
+++ app/controller/api.go
@@ -0,0 +1,16 @@
+package controller
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+//apiVersion is a simple endpoint handler that
+//just writes a simple struct as json as the HTTP response.
+func ApiVersion(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(
+		struct {
+			Name    string `json:"name"`
+			Version string `json:"version"`
+		}{"PTC-Search-Service", "0.1"})
+}
