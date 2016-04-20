6adbddebdd8c5e274fff595ab4d7673d81c1cdde
David Skeppstedt <david.skeppstedt@gmail.com>
2016-04-20 17:37:36 +0200

    Code refactoring! Jag bröt upp den stora mastodånt funktionen  func (this *Elastic) GetTweetDateForUser(user_id string, w http.ResponseWriter)  till flera mindre. I ett första steg så flyttades searchResult buildern till en ny fil döpt till RangeQueries. I sann DRY anda förminskade jag liknande kod till en funktion som numera heter GetTweetDate.
    Funktionallitetn har dock inte ändrats från ursprungskoden förutom möjligheten att matcha mot hela datamängden och inte enbart mot de som följer specifica partier.

--- app/storage/elastic.go
+++ app/storage/elastic.go
@@ -3,18 +3,14 @@ package storage
 import (
 	"log"
 
-	"encoding/json"
 	"gopkg.in/olivere/elastic.v3"
-	"lcd/PTC-search-service/app/models"
-	"net/http"
-	"reflect"
 )
 
 var ElasticSearch Elastic
 
 // A type that holds our Elastic client, to prevent us from creating it over and over again
 type Elastic struct {
-	client *elastic.Client
+	Client *elastic.Client
 }
 
 // Create the elastic client and return a Elastic struct containing the client
@@ -43,7 +39,7 @@ func (this *Elastic) SearchTweetsFromID(user_id string) *elastic.SearchResult {
 	//termQuery := elastic.NewTermQuery("user_id", user_id)
 	topTagsHitsAgg := elastic.NewTopHitsAggregation().Sort("user_id", true).Size(5).FetchSource(true)
 	topTagsAgg := elastic.NewTermsAggregation().Field("hashtags").Size(3).SubAggregation("top_tag_hits", topTagsHitsAgg)
-	searchResult, err := this.client.Search().
+	searchResult, err := this.Client.Search().
 		Index("test-index").               // search in index "twitter"
 		Query(elastic.NewMatchAllQuery()). // specify the query
 		Sort("user_id", true).             // sort by "user" field, ascending
@@ -56,61 +52,5 @@ func (this *Elastic) SearchTweetsFromID(user_id string) *elastic.SearchResult {
 		panic(err)
 	}
 	return searchResult
-
-}
-
-// A function that returns the latest or earliest date for tweet from given account
-func (this *Elastic) GetTweetDateForUser(user_id string, w http.ResponseWriter) {
-	// Our return values
-	var firstDate, lastDate string
-
-	// Search for the earliest tweet from a person that is following the given user_id
-	termQuery := elastic.NewTermQuery("following", user_id)
-	searchResult, err := this.client.Search().
-		Index("tweets").    // search in index "tweets"
-		Query(termQuery).   // specify the query
-		Sort("date", true). // sort by "date" field, ascending ==> true
-		From(0).Size(1).    // take document 0
-		Pretty(true).       // pretty print request and response JSON
-
-		Do() // execute
-	if err != nil {
-		// Handle error
-		panic(err)
-	}
-	var ttyp models.Tweet
-	// Loop through the results
-	for _, item := range searchResult.Each(reflect.TypeOf(ttyp)) {
-		if t, ok := item.(models.Tweet); ok {
-			firstDate = t.Date
-		}
-	}
-	// Search for the latest tweet from a person that is following the given user_id
-	searchResult, err = this.client.Search().
-		Index("tweets").     // search in index "twitter"
-		Query(termQuery).    // specify the query
-		Sort("date", false). // sort by "date" field, false => descending
-		From(0).Size(1).     // take document 0
-		Pretty(true).        // pretty print request and response JSON
-
-		Do() // execute
-	if err != nil {
-		// Handle error
-		panic(err)
-	}
-	// Loop through the result
-	for _, item := range searchResult.Each(reflect.TypeOf(ttyp)) {
-		if t, ok := item.(models.Tweet); ok {
-			lastDate = t.Date
-		}
-	}
-
-	// Format JSON and write to response
-	json.NewEncoder(w).Encode(
-		struct {
-			StartDate string `json:"startDate"`
-			EndDate   string `json:"endDate"`
-		}{firstDate, lastDate})
-	return
 
 }
