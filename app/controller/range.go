e7df9a953443f9d6a381ddd9e28129a5eef23d53
David Skeppstedt <david.skeppstedt@gmail.com>
2016-04-24 20:08:19 +0200

    Clean up, removed dead code.

--- app/controller/range.go
+++ app/controller/range.go
@@ -11,32 +11,12 @@ import (
 // returns first and last tweet from a specific party
 // in a json object.
 func TweetRange(w http.ResponseWriter, r *http.Request) {
-
-	type account struct {
-		id string
-	}
-
 	accountId, err := web.Param(r, "account")
 	if err != nil {
-		log.Println("Could not fetch param 'account'")
-		log.Println(err)
+		log.Println("No 'account' specified. Date range will be for whole dateset.")
 	}
-
-	//acc := account{accountID}
-
-	//storage.ElasticSearch.GetTweetDateForUser(accountID, w)
 
 	var rng = models.Range{}
 	rng.GetRange(accountId)
 	json.NewEncoder(w).Encode(rng)
-
-	//party will be sent to elastic and hopfully we can
-	//retrive first and last post from that party
-	//and then return
-	/*json.NewEncoder(w).Encode(
-	struct {
-		Party   string `json:"party"`
-		MinDate string `json:"minDate"`
-		MaxDate string `json:"maxDate"`
-	}{p.name, "2006-02-01T00:00:00.000Z", "2016-02-01T00:00:00.000Z"})*/
 }
