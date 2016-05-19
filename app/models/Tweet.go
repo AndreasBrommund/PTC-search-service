6017c7580f86c0b57316c6934fa05816ed0664ca
David Skeppstedt <david.skeppstedt@gmail.com>
2016-05-19 09:31:14 +0200

    Cleaned up the Tweet model struct and added comments.

--- app/models/Tweet.go
+++ app/models/Tweet.go
@@ -4,19 +4,15 @@ import (
 	"gopkg.in/olivere/elastic.v3"
 )
 
-type Retweet struct {
-	RetweetID string `json:"retweet_id"`
-	CreatorID string `json:"creator_id"`
-}
-
+//Tweet a model struct that represents a typical
+//tweet in elastic.
 type Tweet struct {
-	UserID    string   `json:"user_id"`
-	TweetID   string   `json:"tweet_id"`
-	Text      string   `json:"text"`
-	Hashtags  []string `json:"hashtags"`
-	Mentions  []string `json:"mentions"`
-	Langugage string   `json:"lang"`
-	//Retweets  []Retweet             `json:"RT"`
-	Date    string                `json:"date"`
-	Suggest *elastic.SuggestField `json:"suggest_field,omitempty"`
+	UserID    string                `json:"user_id"`
+	TweetID   string                `json:"tweet_id"`
+	Text      string                `json:"text"`
+	Hashtags  []string              `json:"hashtags"`
+	Mentions  []string              `json:"mentions"`
+	Langugage string                `json:"lang"`
+	Date      string                `json:"date"`
+	Suggest   *elastic.SuggestField `json:"suggest_field,omitempty"`
 }
